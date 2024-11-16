7563796bf7b24d6abc6f31a25b52c16d7738ffc6
Shira Boodaei <shiraboodaei@Shiras-MacBook-Air.local>
2024-11-16 13:59:39 +0200

    Create calc pi class

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,22 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    // Get user input 
+		int numOfTerms = Integer.parseInt(args[0]);
+		// calc 
+		double denominator = 3;
+		double finalSum = 1;
+		for (int i = 0; i < numOfTerms - 1; i++) {
+				if (i%2 == 0) {
+					finalSum = finalSum - (1/denominator);
+				} else {
+					finalSum = finalSum + (1/denominator);
+				}
+				denominator = denominator + 2;
+		}
+		double piAccordingToJava = 4 * (finalSum);
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated: " + piAccordingToJava);
+
 	}
 }
